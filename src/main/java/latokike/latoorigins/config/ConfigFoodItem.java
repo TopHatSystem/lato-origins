eb8eae4c9fcc00d950bf61add0bfd8d4d74d5ed6
TopHatSystem <80288473+TopHatSystem@users.noreply.github.com>
2021-06-26 23:09:30 +0200

    Update ConfigFoodItem.java

--- src/main/java/latokike/latoorigins/config/ConfigFoodItem.java
+++ src/main/java/latokike/latoorigins/config/ConfigFoodItem.java
@@ -3,11 +3,11 @@ package latokike.latoorigins.config;
 public class ConfigFoodItem {
     public String itemId;
     public Integer hungerShanks;
-    public Float saturation;
+    public Float saturation;0
 
     public ConfigFoodItem(String itemId, Integer hungerShanks, Float saturation) {
         this.itemId = itemId;
         this.hungerShanks = hungerShanks;
-        this.saturation = saturation;
+        this.saturation = saturation;0
     }
 }
